98f87c2f36d6476aced6dac7958a70603291d149
Francisco A. Galván Báez <francisco.galvan.baez@gmail.com>
2016-03-17 21:39:39 +0000

    Hago algunos arreglillos en las plantillas y adapto el theme a django

--- web/urls.py
+++ web/urls.py
@@ -1,10 +1,34 @@
 __author__ = 'fgalvan'
 
 from django.conf.urls import url
-from web.views import IndexView
+from web.views import *
 
 urlpatterns = [
 
     url(r'^$', IndexView.as_view(template_name='web/index.html'), name='web_index'),
+
+    url(r'^blank/$', BlankView.as_view(template_name='web/blank.html'), name='web_blank'),
+
+    url(r'^buttons/$', ButtonsView.as_view(template_name='web/buttons.html'), name='web_buttons'),
+
+    url(r'^flot/$', FlotView.as_view(template_name='web/flot.html'), name='web_flot'),
+
+    url(r'^forms/$', FormsView.as_view(template_name='web/forms.html'), name='web_forms'),
+
+    url(r'^grid/$', GridView.as_view(template_name='web/grid.html'), name='web_grid'),
+
+    url(r'^icons/$', IconsView.as_view(template_name='web/icons.html'), name='web_icons'),
+
+    url(r'^login/$', LoginView.as_view(template_name='web/login.html'), name='web_login'),
+
+    url(r'^morris/$', MorrisView.as_view(template_name='web/morris.html'), name='web_morris'),
+
+    url(r'^notifications/$', NotificationsView.as_view(template_name='web/notifications.html'), name='web_notifications'),
+
+    url(r'^panels-wells/$', PanelsWellsView.as_view(template_name='web/panels-wells.html'), name='web_panelswells'),
+
+    url(r'^tables/$', TablesView.as_view(template_name='web/tables.html'), name='web_tables'),
+
+    url(r'^typography/$', TypographyView.as_view(template_name='web/typography.html'), name='web_typography  '),
 
 ]
--- web/views.py
+++ web/views.py
@@ -6,3 +6,51 @@ from django.views.generic import TemplateView
 # Create your views here.
 class IndexView(TemplateView):
     pass
+
+
+class BlankView(TemplateView):
+    pass
+
+
+class ButtonsView(TemplateView):
+    pass
+
+
+class FlotView(TemplateView):
+    pass
+
+
+class FormsView(TemplateView):
+    pass
+
+
+class GridView(TemplateView):
+    pass
+
+
+class IconsView(TemplateView):
+    pass
+
+
+class LoginView(TemplateView):
+    pass
+
+
+class MorrisView(TemplateView):
+    pass
+
+
+class NotificationsView(TemplateView):
+    pass
+
+
+class PanelsWellsView(TemplateView):
+    pass
+
+
+class TablesView(TemplateView):
+    pass
+
+
+class TypographyView(TemplateView):
+    pass
