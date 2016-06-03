a1c5c5fd58363e6e5d4394ee35aa9297fa0db7fb
Francisco A. Galván Báez <francisco.galvan.baez@gmail.com>
2016-06-03 21:07:29 +0100

    Algunos avances con selenium mas

--- scraping/scraper_selenium.py
+++ scraping/scraper_selenium.py
@@ -2,8 +2,11 @@
 from bs4 import BeautifulSoup
 from web.models import Host
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 import os
-import requests
+
 
 chromedriver = "/usr/bin/chromedriver"
 os.environ["webdriver.chrome.driver"] = chromedriver
@@ -13,48 +16,111 @@ driver_firefox = webdriver.Firefox()
 # driver = webdriver.Firefox()
 #url = "https://www.tripadvisor.es"
 
-driver_firefox.implicitly_wait(10)
-
-
 def scrap_tripadvisor(link):
 
     try:
         driver_firefox.get(link)
+        i = 1
+        hotels_n = driver_firefox.find_elements_by_class_name('listing')
+
+        # Elimino el hotel patrocinado
+        if len(hotels_n) > 30:
+            hotels_n.pop(0)
+
+
+        print len(hotels_n)
+        for h in hotels_n:
+            try:
+                hotel = h.find_element_by_class_name('property_title')
+                name = hotel.text
+                print hotel.text + "  ------  " + str(i)
+                code = hotel.get_attribute('id')
+                # print code
+                total = h.find_element_by_class_name('sprite-ratings').get_attribute('alt')
+                rate = total.split(' ')
+
+                ranking = h.find_element_by_class_name('slim_ranking').text
+                print ranking
+
+                """host = Host.objects.create(type='H', code=code, name=name, sleep_quality=1, location=1, rooms=1,
+                    services=1, quality_price=1, cleaning=1, total=rate[0])
+
+                host.save()"""
+
+                i += 1
+
+            except Exception as e:
+                print e
+
         next = driver_firefox.find_element_by_class_name("next")
 
-        print len(next.get_attribute('href'))
-        while len(next.get_attribute('href')) > 0:
+        pag = 1
+        while next is not None:
 
-            hotels_n = driver_firefox.find_elements_by_class_name('listing')
+            pag += 1
+            driver_firefox.get(next.get_attribute('href'))
+            hotels_n = driver_firefox.find_elements_by_class_name('listing_info')
+
+
+            #Elimino el hotel patrocinado
+            if len(hotels_n) > 30:
+                hotels_n.pop(0)
+
+            print len(hotels_n)
+
             for h in hotels_n:
+                driver_firefox.implicitly_wait(10)
                 try:
                     hotel = h.find_element_by_class_name('property_title')
-
+                    i += 1
                     name = hotel.text
-                    print hotel.text
-
                     code = hotel.get_attribute('id')
-                    print code
 
-                    total = h.find_element_by_class_name('sprite-ratings').get_attribute('alt')
-                    rate = total.split(' ')
+                    try:
+                        total = h.find_element_by_class_name('sprite-ratings').get_attribute('alt')
+                    except Exception as e:
+                        print e
+
+                    try:
+                        rate = total.split(' ')
+                    except Exception as e:
+                        print e
+
+                    try:
+                        ranking = h.find_element_by_class_name('slim_ranking').text
+                    except Exception as e:
+                        print e
 
                     """host = Host.objects.create(type='H', code=code, name=name, sleep_quality=1, location=1, rooms=1,
                         services=1, quality_price=1, cleaning=1, total=rate[0])
 
                     host.save()"""
 
-                except Exception as e:
-                    print e
+                    print name + "  ------  " + str(i)
+                    print code
+                    print rate[0]
+                    print ranking
 
-            print next.get_attribute('href')
+                except Exception as e:
+
+                    i += 1
+                    print e
+                    print "Estoy en el while capullo"
+
             try:
-                driver_firefox.get(next.get_attribute('href'))
-                #driver_firefox.implicitly_wait(5)
+                next = driver_firefox.find_element_by_class_name("next")
+
+                if "disable" in next.get_attribute('class'):
+                    next = None
+
             except Exception as e:
                 print e
+                print "Estoy fuera del while"
+
+            print "*********************" + str(pag)
 
     except Exception as e:
         print e
+        print "Estoy saliendo del primer try"
 
     driver_firefox.close()
